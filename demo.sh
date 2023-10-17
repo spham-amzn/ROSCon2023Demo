ac4663ac22a6c8baf6f43dc26798bd93cfb93c03
Steve Pham <82231385+spham-amzn@users.noreply.github.com>
2023-10-17 08:06:19 -0700

    Add demo script
    Signed-off-by: Steve Pham <82231385+spham-amzn@users.noreply.github.com>

---
+++ demo.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+COMMAND=$1
+ROBOT=$2
+BASE=/home/o3de/github/ROSCon2023Demo
+
+if [ "$COMMAND" = "" ]
+then
+    echo "ROSConDemo2023 Script usage:"
+    echo "./demo.sh [COMMAND] [ROBOT]"
+    echo
+    echo "COMMAND (launch|spawn|rviz|editor|build)"
+    echo
+    echo "Example:  Launch the Simulation:"
+    echo "  ./demo.sh launch"
+    echo
+    echo "Example:  Launch the Editor:"
+    echo "  ./demo.sh editor"
+    echo
+    echo "Example:  Build:"
+    echo "  ./demo.sh build"
+    echo
+    exit 0
+fi
+
+echo Command $COMMAND
+echo Robot $ROBOT
+
+source /opt/ros/humble/setup.bash
+
+if [ "$COMMAND" = "launch" ]
+then
+    # Kill any AssetBuilder Process
+    for i in $(ps -ef | grep AssetBuilder | grep -v grep | awk '{print $2}')
+    do
+        echo Killing AssetBuilder $i
+        kill -9 $i
+    done
+
+    ros2 daemon stop
+
+    ros2 daemon start
+
+    echo Launching ROSCon2023 Demo Simulation
+
+    $BASE/Project/build/linux/bin/profile/./ROSCon2023Demo.GameLauncher -bg_ConnectToAssetProcessor=0 > /dev/null
+    exit $?
+
+elif [ "$COMMAND" = "rviz" ]
+then
+
+    cd $BASE/ros2_ws
+
+    source install/setup.bash
+
+    ros2 launch roscon2023_demo ROSCon2023Demo.launch.py
+
+elif [ "$COMMAND" = "editor" ]
+then
+    ros2 daemon stop
+
+    ros2 daemon start
+
+    echo Launching Editor for ROSCon2023 Demo
+
+    $BASE/Project/build/linux/bin/profile/Editor > /dev/null
+    exit $?
+elif [ "$COMMAND" = "build" ]
+then
+
+    cd $BASE/ros2_ws
+
+    colcon build --symlink-install
+
+    cd $BASE
+    
+    cmake -B $BASE/Project/build/linux -G "Ninja Multi-Config" -S $BASE/Project -DLY_DISABLE_TEST_MODULES=ON -DLY_STRIP_DEBUG_SYMBOLS=ON -DAZ_USE_PHYSX5=ON
+    if [ $? -ne 0 ]
+    then
+        echo "Error building"
+        exit 1
+    fi
+
+    cmake --build $BASE/Project/build/linux --config profile --target Editor ROSCon2023Demo.GameLauncher ROSCon2023Demo.Assets
+
+    exit 0 
+
+else
+    echo "Invalid Command $COMMAND"
+fi
+
